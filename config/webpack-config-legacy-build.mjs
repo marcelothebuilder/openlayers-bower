46d34ab1e1b2a3afe349f81ad2ff53a9057b037b
Marcelo Paixão Resende <marcelo.rezende@callink.com.br>
2023-10-03 16:51:50 -0300

    build without obfuscating names

--- config/webpack-config-legacy-build.mjs
+++ config/webpack-config-legacy-build.mjs
@@ -36,16 +36,6 @@ export default {
       ol: path.resolve('./build/ol'),
     },
   },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          // Mangle private members convention with underscore suffix
-          mangle: {properties: {regex: /_$/}},
-        },
-      }),
-    ],
-  },
   output: {
     path: path.resolve('./build/legacy'),
     publicPath: 'auto',
